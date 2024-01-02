1b12cc1137832620cb1411b60e2e07e18e7dca71
Ali <155351577+userali-ir@users.noreply.github.com>
2024-01-02 17:12:22 +0330

    list commit

---
+++ Theme/js/script.js
@@ -0,0 +1,35 @@
+/* By > www.userali.ir */
+
+(function($){
+"use strict";
+
+/*========== Loader start ================*/
+$(window).on('load', function() {
+	$('#loader-wrapper').fadeIn();
+	setTimeout(function() {
+		$('#loader-wrapper').fadeOut();
+	}, 500);
+});
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+});
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
+})(jQuery);
+
+/* By > www.userali.ir */
